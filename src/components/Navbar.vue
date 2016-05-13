6dec4aa6ad57fc93c4c381e498b284833269f3cc
xyxiao <xyxiao>
2016-05-13 18:09:08 +0800

    add dashboard

---
+++ src/components/Navbar.vue
@@ -0,0 +1,122 @@
+<template>
+  <section class="menu">
+    <nav class="left">
+      <ul>
+        <li v-for="menu in menus">
+          <a>{{menu}}</a>
+        </li>
+      </ul>
+    </nav>
+    <section id="logo">
+      <a class="logo" v-link="{name: Dashboard}">摄影爱好者 <span>图片聚集地！</span></a>
+    </section>
+    <nav class="right">
+      <ul>
+        <li>
+          <a>注册</a>
+        </li>
+        <li>
+          <a>登陆</a>
+        </li>
+      </ul>
+    </nav>
+  </section>
+</template>
+
+<style lang="scss">
+   section.menu {
+     height: 85px;
+     background-color: #0c0d0d;
+     color: #b4b9b9;
+     transition: all 0.3s ease-out;
+
+     nav.left {
+       padding: 0;
+       width: 50%;
+       float: left;
+     }
+
+     nav.right {
+       padding: 0;
+       width: 50%;
+       float: right;
+
+       ul li {
+         float: right;
+
+         a {
+           color: white;
+           opacity: 0.9;
+         }
+
+         a:hover {
+           color: #02a388;
+         }
+       }
+     }
+
+     li {
+       display: inline-block;
+       font-size: 1rem;
+       line-height: 85px;
+       padding: 0 30px;
+       white-space: nowrap;
+
+       a {
+         transition: all 0.3s ease-out;
+         color: #b4b9b9;
+       }
+
+       a:hover {
+         color: #02a388;
+       }
+     }
+
+     #logo {
+       height: 0;
+       margin: 0 auto;
+       position: absolute;
+       text-align: center;
+       width: 100%;
+
+       a {
+         display: block;
+         width: 200px;
+         height: 58px;
+         margin: 0 auto;
+         position: relative;
+         font-size: 26px;
+         color: white;
+         padding-top: 10px;
+         transition: all 0.5s;
+
+         span {
+           margin: auto;
+           display: block;
+           font-size: 15px;
+           color: white;
+           opacity: 0.9;
+           transition: all 0.5s;
+         }
+       }
+       a.logo:hover {
+         opacity: 0.5;
+       }
+
+       a.logo:hover span{
+         opacity: 0.4;
+       }
+     }
+   }
+</style>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        menus: ['照片', '插画', '视频', '音乐']
+      }
+    }
+  }
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,91 @@
 <template>
-  首页
+  <Navbar></Navbar>
+  <section class="show">
+    <section class="search">
+      <div class="icon">
+        <i class="fa fa-search fa-2x"></i>
+      </div>
+      <div class="searchBar">
+        <input type="text" name="name" value="">
+      </div>
+    </section>
+  </section>
 </template>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+    min-width: 1024px;
+    min-height: 1000px;
+
+    li {
+      list-style: none;
+    }
+
+    a {
+      cursor: pointer;
+    }
+
+    a:hover {
+      text-decoration: none;
+    }
+
+    .show {
+      width: auto;
+      height: 400px;
+      background-size: cover;
+      background-image: url(/show.jpg);
+
+      .search {
+        width: 900px;
+        height: 72px;
+        top: 100px;
+        position: relative;
+        background-color: white;
+        margin: auto;
+
+        .icon {
+          display: inline-block;
+          width: 20%;
+          height: 100%;
+          float: left;
+
+          i {
+            padding-left: 20px;
+            padding-top: 5px;
+            opacity: 0.8;
+          }
+        }
+      }
+
+      .searchBar {
+        display: inline-block;
+        width: 60%;
+        height: 100%;
+        float: left;
+
+      }
+
+    }
+  }
+</style>
+
+<script>
+  // 导入模块
+  import Navbar from 'Navbar'
+  import $ from 'jquery'
+
+  export default {
+    data () {
+      return {
+      }
+    },
+    components: {
+      Navbar
+    },
+    ready () {
+      console.log($)
+    }
+  }
+</script>
