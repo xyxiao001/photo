2cfae038f83bc4a0e5a30f74f1c7b5a71f40df84
xyxiao <463618622@qq.com>
2016-05-17 21:26:26 +0800

    finish photo page

--- src/components/Waterfall.vue
+++ src/components/Waterfall.vue
@@ -1,44 +1,104 @@
 <template>
   <div class="waterfall">
-    <div class="waterRow">
-      <img src="../assets/photo1.jpg" alt="" />
-      <img src="../assets/photo2.jpg" alt="" />
-      <img src="../assets/photo1.jpg" alt="" />
-      <img src="../assets/photo2.jpg" alt="" />
-      <img src="../assets/photo1.jpg" alt="" />
-      <img src="../assets/photo2.jpg" alt="" />
-      <img src="../assets/show.jpg" alt="" />
-      <img src="../assets/photo1.jpg" alt="" />
-      <img src="../assets/photo2.jpg" alt="" />
-      <img src="../assets/show.jpg" alt="" />
+    <div class="show-photo">
+      <div class="d-photo" v-for="img in imgs">
+        <img v-bind:src="img.src">
+        <a>{{img.title}}</a>
+      </div>
+    </div>
+    <div class="show-more">
+      <a v-on:click="showMore">展示更多</a>
     </div>
   </div>
 </template>
 
 <style  lang="scss">
-  img {
-    margin: 8px;
-    width: auto;
-    height: 180px;
+  .waterfall {
+    padding-right: 20px;
+    padding-left: 20px;
   }
 
-  @media screen and (min-width: 1920px) {
+  @media screen and (max-width: 1250px)  {
+    .waterfall {
+      padding-left: 0;
+    }
+  }
+
+  .d-photo {
+    display: inline-block;
+    height: 230px;
+    max-width: 23%;
+    margin: 8px 10px;
+    cursor: pointer;
+    overflow: hidden;
+    transition: all 0.3s ease-out;
+
     img {
-      height: 250px;
+      width: 100%;
+      height: 100%;
+    }
+
+    a {
+      position: relative;
+      top: -230px;
+      display: inline-block;
+      height: 230px;
+      width: 100%;
+      opacity: 0;
+      color: black;
+      transition: all 0.3s ease-out;
+
+    }
+  }
+
+  .d-photo:hover img{
+    opacity: 0.3;
+  }
+
+  .d-photo:hover a {
+    opacity: 1;
+    padding-left: 50px;
+  }
+
+  .show-more {
+    width: 100%;
+    height: 50px;
+    text-align: center;
+
+    a {
+      display: inline-block;
+      width: 80px;
+      height: 30px;
+      color: black;
+      margin: auto;
+      padding: 2px;
+      border: 1px solid black;
+      border-radius: 5px;
+    }
+
+    a:hover {
+      color: #028a71;
     }
   }
 </style>
 
 <script>
-  import One from '../assets/photo1.jpg'
-  import Two from '../assets/photo2.jpg'
-  import Three from '../assets/photo3.jpg'
-  import Four from '../assets/photo4.jpg'
-
   export default {
-    data () {
-      return {
-        img: [One, Two, Three, Four]
+    props: ['imgs'],
+    methods: {
+      showMore () {
+        for (var i = 0; i < 10; i++) {
+          var f = parseInt(Math.random() * this.imgs.length - 1)
+          var div = document.createElement('div')
+          div.className = 'd-photo'
+          var img = document.createElement('img')
+          img.src = this.imgs[f].src
+          var a = document.createElement('a')
+          a.innerHTML = this.imgs[f].title
+          div.appendChild(img)
+          div.appendChild(a)
+          document.getElementsByClassName('show-photo')[0].appendChild(div)
+        }
       }
     }
   }
--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -2,7 +2,7 @@
   <Navbar></Navbar>
   <section class="page-content">
     <Search :placeholder="placeholder" choose="图片"></Search>
-    <WaterFall></Waterfall>
+    <WaterFall :imgs="imgs"></Waterfall>
   </section>
   <foot></foot>
 </template>
@@ -20,9 +20,10 @@
 
       .option {
         width: 20%;
-        float: right;
+        position: absolute;
+        top: 72px;
+        right: 0;
         z-index: 999;
-        top: 0;
 
         ul {
           float: right;
@@ -41,10 +42,37 @@
   import Search from 'Search'
   import Waterfall from 'Waterfall'
 
+  import One from '../assets/photo1.jpg'
+  import Two from '../assets/photo2.jpg'
+  import Three from '../assets/photo3.jpg'
+  import Four from '../assets/photo4.jpg'
+
   export default {
     data () {
       return {
-        placeholder: 'hi ! 想找到什么图片'
+        placeholder: 'hi ! 想找到什么图片',
+        imgs: [
+          {src: One, title: '体验VR, 炫酷的体验'},
+          {
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_ceugbpm7bb2-SxDi3dsmcMpqusqvmAvL9QUqMWCX1RL-5qJY',
+            title: '好萌，好萌的'
+          },
+          {
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXQ118ASvPuMDxDOIL9dAq5MSjx71eX0YCFsd8ijxEwpMqTooz',
+            title: '这是谁 好想知道'
+          },
+          {src: Two, title: '纹理， 还不错哟'},
+          {
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPmHRdfF5bMyxS6K-ZawTItxqRZyW5ery3e1rSAjFkzqkhnNz8Cw',
+            title: '美丽的晚霞'
+          },
+          {
+            src: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS3sVVUtnTDrv0lObNUy6AG6iILM2qo4lhEr7yMkqirEQfFDh28',
+            title: '埃菲尔铁塔'
+          },
+          {src: Three, title: '大灰狼和小红帽的故事'},
+          {src: Four, title: '拿着iPad学习的'}
+        ]
       }
     },
     components: {
